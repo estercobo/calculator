6021f503de3827d8eebe17b4d70c443a50716452
Ester Cobo <ecobo1985@gmail.com>
2019-05-19 19:53:28 +0200

    Add test for sum function when there is no number2: fails

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -3,12 +3,22 @@ from ..src.calculator import *
 class TestCalculator:
 
     @staticmethod
-    def testSum():
+    def test_sum():
         # arrange
         number1 = 1
         number2 = 2
         expectedSum = 3
         # act
         sum = Calculator.sum(number1, number2)
+        # assert
+        assert sum == expectedSum
+
+    @staticmethod
+    def test_sum_without_number2():
+        # arrange
+        number1 = 1
+        expectedSum = 1
+        # act
+        sum = Calculator.sum(number1)
         # assert
         assert sum == expectedSum
